611584d7a4abd8618107bb7e8637f7f269eb3bfb
Grigory-Aseev <grinodin7@gmail.com>
2023-03-14 14:22:51 +0500

    Implement F# interpret with active patterns
    Signed-off-by: Grigory-Aseev <grinodin7@gmail.com>

---
+++ FSharpActivePatterns/demos/demo.ml
@@ -0,0 +1,8 @@
+(** Copyright 2022-2023, Grigory Aseev *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+(* open FSharpActivePatterns_lib.Parser *)
+open FSharpActivePatterns_lib.Interpret
+
+let () = run (Stdio.In_channel.input_all Caml.stdin)
